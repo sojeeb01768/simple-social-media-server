876e65f15cfd7c899c0e77d92ba2a52f59466094
Md.Al kafee <sojeebahmed.sa@gmail.com>
2022-12-27 17:40:40 +0600

    social media server

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kvqywrf.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('socisal media server is running')
+})
+
+app.listen(port, () => {
+    console.log(`docial media server is running on port ${port}`);
+})
