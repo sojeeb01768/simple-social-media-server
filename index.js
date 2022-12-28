090e27e05910dbc49c824ebcb26150e499132b70
Md.Al kafee <sojeebahmed.sa@gmail.com>
2022-12-29 02:24:07 +0600

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -14,12 +14,56 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kvqywrf.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
 
+async function run() {
+    try {
+        const postCollection = client.db('socialMedia').collection('posts');
+        const commentCollection = client.db('socialMedia').collection('comments');
+
+
+        app.post('/posts', async (req, res) => {
+            const post = req.body;
+            const result = await postCollection.insertOne(post);
+            res.send(result);
+
+        });
+        app.post('/comments', async (req, res) => {
+            const comments = req.body;
+            const result = await commentCollection.insertOne(comments);
+            res.send(result);
+
+        });
+
+        app.get('/posts', async (req, res) => {
+            const query = {};
+            const posts = await postCollection.find(query).toArray();
+            res.send(posts);
+        });
+        app.get('/comments/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {
+                _id: ObjectId(id)
+            };
+            const comment = await commentCollection.findOne(query);
+            res.send(comment);
+        });
+
+        app.get('/showdetails/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {
+                _id: ObjectId(id)
+            };
+            const postResult = await postCollection.findOne(query);
+            res.send(postResult);
+        })
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(err => console.error(err));
 
 
 
