8c9fa46c3f9ef91448b583786ab54faec4e06b90
Md.Al kafee <sojeebahmed.sa@gmail.com>
2022-12-29 18:20:53 +0600

    edit profile

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ async function run() {
     try {
         const postCollection = client.db('socialMedia').collection('posts');
         const commentCollection = client.db('socialMedia').collection('comments');
+        const profileCollection = client.db('socialMedia').collection('profiles');
 
 
         app.post('/posts', async (req, res) => {
@@ -34,21 +35,53 @@ async function run() {
 
         });
 
+        // post user profile to the database
+        app.post('/profiles', async (req, res) => {
+            const user = req.body;
+            // console.log(user);
+            const result = await profileCollection.insertOne(user);
+            res.send(result);
+
+        });
+
+        // get user profile data from database
+        app.get('/profiles/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const profile = await profileCollection.findOne(query);
+            res.send(profile);
+        });
+
+
+        // update profile
+        app.put('/profiles/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log(id);
+            const filter = { _id: ObjectId(id) };
+            const profile = req.body;
+            // console.log(profile);
+            const option = { upsert: true };
+            const updatedProfile = {
+                $set: {
+                    name: profile.name,
+                    email: profile.email,
+                    university: profile.university,
+                    address: profile.address
+                }
+            }
+            const result = await profileCollection.updateOne(filter, updatedProfile, option);
+            res.send(result);
+
+        });
+
         app.get('/posts', async (req, res) => {
             const query = {};
             const posts = await postCollection.find(query).toArray();
             res.send(posts);
         });
 
-    
-        // app.get('/comments/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = {
-        //         _id: ObjectId(id)
-        //     };
-        //     const comment = await commentCollection.findOne(query);
-        //     res.send(comment);
-        // });
+
+
 
         app.get('/comments', async (req, res) => {
             let query = {};
